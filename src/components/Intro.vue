009c1ae5d2de1e3d0696fc955581721c4f99bc56
Gilbert Nigg <tech@gilles.ch>
2019-12-30 23:01:16 +0100

    First component

---
+++ src/components/Intro.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="intro">
+    <h4>
+      Willkommen bei Ihrem Partner für die Um&shy;setzung
+      unter&shy;schied&shy;lichster Web&shy;lösungen. Für Agenturen als auch für
+      Direkt&shy;kunden – von der Konzeption bis zur erfolg&shy;reichen
+      Online-Schaltung und Betreuung.
+    </h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Intro"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
