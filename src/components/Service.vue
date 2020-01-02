b32d5adf64c31636e78fe10f5db98d411a0f4b25
Gilbert Nigg <tech@gilles.ch>
2020-01-03 00:49:31 +0100

    Add some static content

--- src/components/Service.vue
+++ src/components/Service.vue
@@ -3,6 +3,21 @@
     <h2 v-html="titleH2" />
     <h3 v-html="titleH3" />
     <hr />
+    <p>
+      Bei vielen Projekten, die an mich herangetragen werden, geht es um die
+      Programmierung klassischer Website mit Content Management System (CMS) für
+      ein selbständiges Bearbeiten der Inhalte. Erfolgt die Anfrage über eine
+      Agentur für einen ihrer Kunden, ist ein Designvorschlag oder eine
+      Konzeption in der Regel schon vorhanden. In diesem Fall setze ich in
+      Zusammenarbeit mit den Gestaltern den Entwurf in eine moderne Website um.
+      Bei Direktkunden entwickle ich bei Bedarf auch das Design, wenn
+      erforderlich im Team mit ausgewählten Webdesignern aus meinem langjährigen
+      Netzwerk.
+    </p>
+    <p>
+      Für detaillierte Informationen oder spezifische Bedürfnisse, zögern Sie
+      nicht, mich zu <a href="#kontakt">kontaktieren</a>.
+    </p>
   </div>
 </template>
 
--- src/components/UeberMich.vue
+++ src/components/UeberMich.vue
@@ -3,6 +3,18 @@
     <h2 v-html="titleH2" />
     <h3 v-html="titleH3" />
     <hr />
+    <p>
+      Anfangs 2002 habe ich begonnen, als selbständiger Web Developer meine
+      ersten Kunden zu betreuen. Die vorherige Zeit als Mitarbeiter einer
+      Webagentur mit eingerechnet, kann ich getrost von mir behaupten, dass ich
+      die gesamte Entwicklung des kommerziellen Internets miterlebt habe. Nicht
+      von aussen, sondern mittendrin. Meine Berufslehre als Polygraf brachte mir
+      zusätzlich die nötige Schnittstelle zwischen Design und Code. Diese
+      Erfahrung mit immer neuen technischen und gestalterischen
+      Herausforderungen hat mich geprägt und gibt mir die Sicherheit, für meine
+      vielen treuen und neuen Kunden, die jeweils beste Lösungen anbieten zu
+      können.
+    </p>
   </div>
 </template>
 
