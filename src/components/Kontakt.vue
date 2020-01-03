76f10b917ce4246b7b3326fde482dd5ec3a2086c
Gilbert Nigg <tech@gilles.ch>
2020-01-03 18:09:55 +0100

    Add google-maps example

--- src/components/Kontakt.vue
+++ src/components/Kontakt.vue
@@ -3,15 +3,94 @@
     <h2 v-html="titleH2" />
     <h3 v-html="titleH3" />
     <hr />
+    <GoogleMapLoader
+      :mapConfig="mapConfig"
+      apiKey="AIzaSyBsgTVGdbGTsxSTyUxQS491ZJAWj--vraI"
+    >
+      <template slot-scope="{ google, map }">
+        <GoogleMapMarker
+          v-for="marker in markers"
+          :key="marker.id"
+          :marker="marker"
+          :google="google"
+          :map="map"
+        />
+        <GoogleMapLine
+          v-for="line in lines"
+          :key="line.id"
+          :path.sync="line.path"
+          :google="google"
+          :map="map"
+        />
+      </template>
+    </GoogleMapLoader>
   </div>
 </template>
 
 <script>
+import GoogleMapLoader from "./GoogleMapLoader";
+import GoogleMapMarker from "./GoogleMapMarker";
+import GoogleMapLine from "./GoogleMapLine";
+
+import { mapSettings } from "@/constants/mapSettings";
 export default {
   name: "Kontakt",
   props: {
     titleH2: String,
     titleH3: String
+  },
+
+  components: {
+    GoogleMapLoader,
+    GoogleMapMarker,
+    GoogleMapLine
+  },
+  data() {
+    return {
+      markers: [
+        {
+          id: "a",
+          position: { lat: 3, lng: 101 }
+        },
+        {
+          id: "b",
+          position: { lat: 5, lng: 99 }
+        },
+        {
+          id: "c",
+          position: { lat: 6, lng: 97 }
+        }
+      ],
+      lines: [
+        {
+          id: "1",
+          path: [
+            { lat: 3, lng: 101 },
+            { lat: 5, lng: 99 }
+          ]
+        },
+        {
+          id: "2",
+          path: [
+            { lat: 5, lng: 99 },
+            { lat: 6, lng: 97 }
+          ]
+        }
+      ]
+    };
+  },
+
+  computed: {
+    mapConfig() {
+      return {
+        ...mapSettings,
+        center: this.mapCenter
+      };
+    },
+
+    mapCenter() {
+      return this.markers[1].position;
+    }
   }
 };
 </script>
