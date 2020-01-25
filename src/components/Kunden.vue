718efdc3f1cdf817b080a7a7a9c3741bc1d71ae8
Gilbert Nigg <tech@gilles.ch>
2020-01-25 17:43:48 +0100

    Fetch JSON data to Kunden

--- src/components/Kunden.vue
+++ src/components/Kunden.vue
@@ -3,7 +3,14 @@
     <h2 v-html="heading.title" />
     <h3 v-html="heading.subTitle" />
     <hr />
-    [...]
+    <ul v-if="Object.keys(kunden).length">
+      <li v-for="kunde in kunden" :key="kunde.id">
+        {{ kunde.name }}
+      </li>
+    </ul>
+    <p v-else>
+      Lade Daten...
+    </p>
   </div>
 </template>
 
@@ -12,6 +19,16 @@ export default {
   name: "Kunden",
   props: {
     heading: Object
+  },
+  data() {
+    return {
+      kunden: {}
+    };
+  },
+  mounted() {
+    fetch("https://www.gilles.ch/portfolio/kunden.json")
+      .then(response => response.json())
+      .then(data => (this.kunden = data));
   }
 };
 </script>
